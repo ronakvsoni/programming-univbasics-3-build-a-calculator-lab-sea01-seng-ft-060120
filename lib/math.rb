897b451da9770177442e8a118bab9b267bb3f48a
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-22 19:01:01 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,38 @@
 def addition(num1, num2)
-
+num3 = num1 + num2
+return num3
 end
+puts addition(5,4)
+
 
 def subtraction(num1, num2)
-
+num3 = num1 - num2
+return num3
 end
+puts subtraction(10,5)
 
 def division(num1, num2)
-
+  num3 = num1/num2
+  return num3
 end
+puts division(50,2)
+
 
 def multiplication(num1, num2)
-
+num3 = num1*num2
+return num3
 end
+puts multiplication(4,30)
 
 def modulo(num1, num2)
-
+  num3= num1%num2
+  return num3
 end
+puts modulo(34,5)
 
 def square_root(num)
-
+root = Math.sqrt(num)
+return root
 end
+
+puts square_root(81)
